156eefa29f31fbcdd58c36db1ce53c29192fa8e6
JuanMontiel300 <montielsebastian555@gmail.com>
2025-12-05 15:10:14 -0500

    Delete Taller Dart/dart_application_1 directory

--- Taller Dart/dart_application_1/bin/1.promedio.dart
+++
@@ -1,9 +0,0 @@
-void main (){
-  var  edad1 = 18;
-  var edad2 = 18;
-  var edad3 = 20;
-  double promediotrespersonas = (edad1 + edad2 + edad3 )/3;
-  print("----------------------------------------------");
-
-  print("El promedio de la edad: $promediotrespersonas");
-}
--- Taller Dart/dart_application_1/bin/47.positivos.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  print("Números impares menores de 100 que NO son divisibles entre 7:");
-
-  for (int i = 1; i < 100; i += 2) {
-    if (i % 7 != 0) {
-      print(i);
-    }
-  }
-}
